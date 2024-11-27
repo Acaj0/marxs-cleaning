17e39d580d18d11aa3a054996b9730d12fa163eb
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-11-27 13:23:29 -0400

    Regular Cleaning finish

--- src/components/services-section.tsx
+++ src/components/services-section.tsx
@@ -22,36 +22,42 @@ const services = [
     description:
       "The rush of everyday life can get in the way of your plans and you end up not having enough time to clean your house. Don't worry, we have the perfect solution for you!",
     icon: Sparkles,
+    link:"/services/regular-cleaning",
   },
   {
     title: "Deep Cleaning",
     description:
       "This type of service is done by capable professionals who know the steps needed to get rid of all the germs, fungi and bacteria around your house. It is the best solution for your environment.",
     icon: Broom,
+    link:"/services/deep-cleaning",
   },
   {
     title: "Move In/Out",
     description:
       "Don't worry about cleaning when moving anymore, Marx's Cleaning puts its professionals at your disposal to rid you of this headache.",
     icon: Home,
+    link:"/services/move-in-out",
   },
   {
     title: "Vacation Home",
     description:
       "Preparing for new guests? From meticulous cleaning to refreshing linens, we transform your space into a welcoming haven, setting the stage for a memorable stay. Trust Marx's Cleaning to elevate the guest experience in your vacation home.",
     icon: Palmtree,
+    link:"/services/vacation-home",
   },
   {
     title: "Post Construction",
     description:
       "The best solution to get rid of all debris created by a construction project. Our team is ready to eliminate all dirt and debris in your property quickly.",
     icon: HardHat,
+    link:"/services/post-construction",
   },
   {
     title: "Commercial",
     description:
       "Keeping your commercial space clean is one way to raise productivity! Not to mention that it keeps your space free of germs and bacteria.",
     icon: Building2,
+    link:"/services/commercial",
   },
 ];
 
@@ -73,9 +79,9 @@ export function ServicesSection() {
                 <CardDescription>{service.description}</CardDescription>
               </CardContent>
               <CardFooter className="mt-auto">
-                <Button variant="outline" className="w-full">
+                <a href={`${service.link}`}><Button variant="outline" className="w-full" >
                   Learn More
-                </Button>
+                </Button></a>
               </CardFooter>
             </Card>
           ))}
